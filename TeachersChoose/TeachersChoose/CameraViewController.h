12e3ddb1688b84c4f2b73a17a9fdff274da7b46d
TOMalley104 <tsto104@gmail.com>
2015-04-01 10:51:55 -0400

    setup branch / tutorial basics

---
+++ TeachersChoose/TeachersChoose/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  TeachersChoose
+//
+//  Created by Tom OMalley on 4/1/15.
+//  Copyright (c) 2015 ZandTheDMs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
