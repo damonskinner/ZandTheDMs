3d1786af76d22cea6640c59ac7f1d0e995ac8c88
movingdecimals <ian@movingdecimals.com>
2015-04-09 13:08:08 -0400

    removed commented code, etc

--- TeachersChoose/StatDetailsViewController.m
+++ TeachersChoose/StatDetailsViewController.m
@@ -40,11 +40,10 @@
 	}
 
 	self.view.backgroundColor = [UIColor whiteColor];
-//    self.view.backgroundColor = [UIColor greenColor];
 
 	self.proposal = ((DetailsTabBarController *)self.tabBarController).selectedProposal;
 
-	NSDictionary *views = @{ @"title" : self.titleLabel,
+	NSDictionary *views = @{ @"titleLabel" : self.titleLabel,
 		                     @"expirationDate" : self.expirationDate,
 		                     @"schoolName" : self.schoolName,
 		                     @"location" : self.location,
@@ -59,36 +58,31 @@
 	self.titleLabel.numberOfLines = 0;
 	self.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
 	self.titleLabel.text = self.proposal.title;
-    self.titleLabel.font = [UIFont fontWithName:DonorsChooseCSSFont size:20];
+    self.titleLabel.font = [UIFont fontWithName:DonorsChooseCSSFont size:24];
 	[self.titleLabel sizeToFit];
 	self.titleLabel.textAlignment = NSTextAlignmentCenter;
-//self.titleLabel.backgroundColor = [UIColor redColor];
-	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[title]-|" options:0 metrics:nil views:views]];
-	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-70-[title(100)]" options:0 metrics:nil views:views]];
+	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[titleLabel]-|" options:0 metrics:nil views:views]];
+	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-70-[titleLabel(100)]" options:0 metrics:nil views:views]];
 
 	self.expirationDate.text = [NSString stringWithFormat:@"Exp. Date: %@", self.proposal.expirationDate];
-//self.expirationDate.backgroundColor = [UIColor blueColor];
 	self.expirationDate.font = [UIFont fontWithName:DonorsChooseBasicFont size:14];
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-25-[expirationDate]-|" options:0 metrics:nil views:views]];
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[location]-30-[expirationDate(30)]" options:0 metrics:nil views:views]];
 
 	self.schoolName.text = [NSString stringWithFormat:@"%@", self.proposal.schoolName];
-//self.schoolName.backgroundColor = [UIColor purpleColor];
     self.schoolName.textAlignment = NSTextAlignmentCenter;
 	self.schoolName.font = [UIFont fontWithName:DonorsChooseBasicFont size:16];
 	self.schoolName.textAlignment = NSTextAlignmentCenter;
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[schoolName]-|" options:0 metrics:nil views:views]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[title][schoolName(10)]" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[titleLabel][schoolName(15)]" options:0 metrics:nil views:views]];
 
 	self.location.text = [NSString stringWithFormat:@"%@, %@ %@", self.proposal.city, self.proposal.state, self.proposal.zip];
-//self.schoolName.backgroundColor = [UIColor purpleColor];
 	self.location.font = [UIFont fontWithName:DonorsChooseBasicFont size:16];
 	self.location.textAlignment = NSTextAlignmentCenter;
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[location]-|" options:0 metrics:nil views:views]];
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[schoolName]-[location(20)]" options:0 metrics:nil views:views]];
 
 	self.fundingStatus.text = [NSString stringWithFormat:@"Funding Status: %@", self.proposal.fundingStatus];
-//self.fundingStatus.backgroundColor = [UIColor brownColor];
 	self.fundingStatus.font = [UIFont fontWithName:DonorsChooseBasicFont size:14];
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-25-[fundingStatus]-|" options:0 metrics:nil views:views]];
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[expirationDate]-[fundingStatus(30)]" options:0 metrics:nil views:views]];
@@ -97,21 +91,16 @@
 	NSInteger total = [self.proposal.totalPrice integerValue];
 	NSString *raisedSoFar = [NSString stringWithFormat:@"%d", total - costToComplete];
 	self.costToCompleteOfTotalWithPercent.text = [NSString stringWithFormat:@"Raised: $%@ of $%@ (%@%%)", raisedSoFar, self.proposal.totalPrice, self.proposal.percentFunded];
-//self.costToCompleteOfTotalWithPercent.backgroundColor = [UIColor yellowColor];
 	self.costToCompleteOfTotalWithPercent.font = [UIFont fontWithName:DonorsChooseBasicFont size:14];
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-25-[costToCompleteOfTotalWithPercent]-|" options:0 metrics:nil views:views]];
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[fundingStatus]-[costToCompleteOfTotalWithPercent(30)]" options:0 metrics:nil views:views]];
 
 	self.numberOfDonations.text = [NSString stringWithFormat:@"Number of Donations: %ld", (unsigned long)[self.proposal.donations count]];
-//self.numberOfDonations.backgroundColor = [UIColor orangeColor];
 	self.numberOfDonations.font = [UIFont fontWithName:DonorsChooseBasicFont size:14];
-	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-25-[numberOfDonations]-|" options:0 metrics:nil views:views]];
+	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-25-[numberOfDonations]" options:0 metrics:nil views:views]];
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[costToCompleteOfTotalWithPercent]-[numberOfDonations(30)]" options:0 metrics:nil views:views]];
-    
-    //[self.shareButton setTitle:@"Share Button" forState:UIControlStateNormal];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[shareButton]-|" options:0 metrics:nil views:views]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[numberOfDonations]-50-[shareButton]" options:0 metrics:nil views:views]];
-    
 }
 
 - (void)createAndAddSubviews {
@@ -142,17 +131,12 @@
     [self.shareButton setTitle:@"Share Button" forState:UIControlStateNormal];
     self.shareButton.frame = CGRectMake(80.0, 210.0, 160.0, 40.0);
     [self.view addSubview:self.shareButton];
-
 }
 
 - (void)shareTapped {
-    
     CustomItemUIActivityItemProvider *customItem = [[CustomItemUIActivityItemProvider alloc] initWithProposal:self.proposal andPlaceholder:@""];
     UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[customItem] applicationActivities:nil];
     [self presentViewController:activityVC animated:YES completion:nil];
 }
-
-
-
 
 @end
