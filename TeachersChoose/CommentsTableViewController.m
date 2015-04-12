37a3e69b399bf06111916f7c16409813e3fd74d7
TOMalley104 <tsto104@gmail.com>
2015-04-12 15:46:57 -0400

    made FISComment custom class and have a textView in the appropriate cell. yet to implement textview delegate methods

--- TeachersChoose/CommentsTableViewController.m
+++ TeachersChoose/CommentsTableViewController.m
@@ -7,67 +7,99 @@
 //
 
 #import "CommentsTableViewController.h"
-#import "ProposalComment.h"
 #import "FISDonorsChooseProposal.h"
 #import "DetailsTabBarController.h"
 #import "FISDonation.h"
+#import "FISComment.h"
 
 
-@interface CommentsTableViewController ()
+@interface CommentsTableViewController () <UITextViewDelegate>
 
 @property (nonatomic, strong) FISDonorsChooseProposal *proposal;
-@property (strong, nonatomic) NSMutableArray *donorsWhoCommented;
-@property (strong, nonatomic) NSMutableDictionary *donorsAndComments;
-
+@property (strong, nonatomic) NSMutableDictionary *commentsDictionary;
+@property (strong, nonatomic) NSMutableArray *orderedListOfDonors;
 @end
 
 @implementation CommentsTableViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    
 
     self.tableView.tableHeaderView = [[UISegmentedControl alloc] initWithItems:@[@"Awaiting Reply", @"All"]];
     ((UISegmentedControl*)self.tableView.tableHeaderView).selectedSegmentIndex = 0;
     
-    self.donorsAndComments = [[NSMutableDictionary alloc] init];
-
-    self.proposal=((DetailsTabBarController*)self.tabBarController).selectedProposal;
+//    self.proposal=((DetailsTabBarController*)self.tabBarController).selectedProposal;
     
-    self.donorsWhoCommented = [[NSMutableArray alloc] init];
-   
-    [self populateCommentsArray];
+    // START FOR TEST PURPOSES ONLY!!!!
     
+    FISDonorsChooseProposal *proposal = [[FISDonorsChooseProposal alloc] init];
     
+    FISDonation *donation = [[FISDonation alloc] init];
+    donation.donorName = @"Edward Billingsworth III";
+    donation.donorMessage = @"i love school sah much";
+    proposal.donations = (NSMutableArray*)@[donation];
+    self.proposal = proposal;
+    
+    // END FOR TEST PURPOSES ONLY!!!!
+    
+    self.commentsDictionary = [[NSMutableDictionary alloc] init];
+    [self populateCommentsDictionary];
 }
 
--(void) populateCommentsArray
+-(void) populateCommentsDictionary
 {
-    for(FISDonation *eachDonation in self.proposal.donations)
+    // work over each donation
+    for(FISDonation *donation in self.proposal.donations)
     {
-        //ProposalComment *newComment = [ProposalComment initWithDonation: eachDonation];
-        //[self.commentsArray addObject:newComment];
+        // declare for further modification
+        FISComment *donorComment;
+        FISComment *teacherResponse;
         
-        if ([eachDonation.donorMessage isEqualToString:@""]) {
-            NSLog(@"nil message");
-        } else {
-            [self.donorsWhoCommented addObject:eachDonation.donorName];
-            [self.donorsAndComments setObject:@[eachDonation.donorMessage] forKey:eachDonation.donorName];
+        // make the appropriate donor comment whether they have it or not
+        if (![donation.donorMessage isEqualToString: @""] || donation.donorMessage
+            )
+        {
+            // if the comment exists, add it
+            donorComment = [[FISComment alloc] initWithDonorComment:donation.donorMessage];
         }
+        else
+        {
+            NSString *automatedComment = [NSString stringWithFormat:@"%@ donated $%@.", donation.donorName, donation.donationAmount];
+            NSLog(@"automatedComment:%@", automatedComment);
+            donorComment = [[FISComment alloc] initWithDonorComment: automatedComment];
+        }
+        
+        // make the appropriate teacher comment whether they have it or not
+        if (donation.responseMessage)
+        {
+            teacherResponse = [[FISComment alloc] initWithTeacherComment:donation.responseMessage];
+        }
+        else
+        {
+            NSString *tapMeText = @"Tap here to reply";
+            teacherResponse = [[FISComment alloc] initWithTeacherComment:tapMeText];
+        }
+        
+        // make an entry in the dictionary with both of these comments in an array
+        [self.commentsDictionary setObject:@[donorComment, teacherResponse] forKey: donation.donorName];
     }
+    
+    NSLog(@"%@", self.commentsDictionary);
+    [self.tableView reloadData];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     //TODO: hook up sections by person
-    return [self.donorsWhoCommented count];
+    return [[self.commentsDictionary allKeys] count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     //TODO: hook up rows by comments per donation / section
-    NSString *donorName = self.donorsWhoCommented[section];
-    NSArray *thisDonorsComments = self.donorsAndComments[donorName];
+    NSString *donorName = [self.commentsDictionary allKeys][section];
+    NSArray *thisDonorsComments = self.commentsDictionary[donorName];
     return [thisDonorsComments count];
 }
 
@@ -76,24 +108,39 @@
     
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"basicCell"];
+    }
+
+    NSString *donorName = [self.commentsDictionary allKeys][indexPath.section];
+    NSArray *thisSetOfComments = self.commentsDictionary[donorName];
+    FISComment *thisComment = thisSetOfComments[indexPath.row];
+    
+    if(thisComment.commentType == CommentFromDonor)
+    {
         cell.textLabel.autoresizingMask = UIViewAutoresizingFlexibleHeight;
         cell.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
         cell.textLabel.numberOfLines = 0;
+        cell.textLabel.text = thisComment.commentBody;
     }
-    
-    NSString *donorName = self.donorsWhoCommented[indexPath.section];
-    NSArray *thisDonorsComments = self.donorsAndComments[donorName];
-    
-    cell.textLabel.text = thisDonorsComments[indexPath.row];
+    else
+    {
+        // make a textview
+        UITextView *myTextView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, cell.frame.size.width, cell.frame.size.height)];
+        myTextView.textAlignment = NSTextAlignmentCenter;
+        myTextView.font = cell.textLabel.font;
+        myTextView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+        myTextView.text = thisComment.commentBody;
+        myTextView.textColor = [UIColor lightGrayColor];
+        
+        // add it to the cell
+        [cell addSubview: myTextView];
+    }
     
     return cell;
 }
 
 -(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return [NSString stringWithFormat:@"%@", self.donorsWhoCommented[section]];
+    return [NSString stringWithFormat:@"%@", [self.commentsDictionary allKeys][section]];
 }
-
-
 
 
 @end
