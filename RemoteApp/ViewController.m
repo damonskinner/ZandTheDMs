51be5b73ae33b952a0b45abbd54f5c16d6c9f409
movingdecimals <ian@movingdecimals.com>
2015-04-15 11:24:30 -0400

    starting remote app

---
+++ RemoteApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RemoteApp
+//
+//  Created by Ian Smith on 4/15/15.
+//  Copyright (c) 2015 Ian Smith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ RemoteApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RemoteApp
+//
+//  Created by Ian Smith on 4/15/15.
+//  Copyright (c) 2015 Ian Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
