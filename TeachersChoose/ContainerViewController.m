92ae41ea3b7dcb71128cb621f460f7a018c83d48
TOMalley104 <tsto104@gmail.com>
2015-04-18 15:50:15 -0400

    FINALLY GOT THE RESIZING TEXT VIEW WORKING

--- TeachersChoose/ContainerViewController.m
+++ TeachersChoose/ContainerViewController.m
@@ -10,12 +10,16 @@
 #import "UIColor+DonorsChooseColors.h"
 #import "UIFont+DonorsChooseFonts.h"
 
-static const float totalNumberOfViewControllers = 4.0;
+static const float TOTAL_VIEW_CONTROLLERS = 4.0;
 
 @interface ContainerViewController () <UINavigationControllerDelegate>
+
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (strong, nonatomic) UINavigationController *navController;
 @property (weak, nonatomic) IBOutlet UIProgressView *myProgressView;
+
+-(void)formatNavBar;
+
 @end
 
 @implementation ContainerViewController
@@ -24,32 +28,41 @@ static const float totalNumberOfViewControllers = 4.0;
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor DonorsChooseOrange];
     self.myProgressView.progressTintColor = [UIColor DonorsChooseOrange];
+    self.myProgressView.trackTintColor = [UIColor DonorsChooseGreyLight];
 }
 
 #pragma mark - Navigation
 
-// THIS IS HOW WE GET THE NAVIGATION CONTROLLER
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    NSLog(@"SOURCE: %@", segue.sourceViewController);
-    NSLog(@"DESTINATION: %@", segue.destinationViewController);
+// get a reference to the embedded nav controller
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+//    NSLog(@"SOURCE: %@", segue.sourceViewController);
+//    NSLog(@"DESTINATION: %@", segue.destinationViewController);
 
     self.navController = segue.destinationViewController;
     self.navController.delegate = self;
-    [self.navController.navigationBar setTranslucent: NO];
-    [self.navController.navigationBar setBarTintColor: [UIColor DonorsChooseOrange]];
-    [self.navController.navigationBar setTitleTextAttributes:@{                                                                    NSForegroundColorAttributeName : [UIColor DonorsChooseGreyVeryLight],NSFontAttributeName:[UIFont fontWithName:DonorsChooseTitleBoldFont size:25]}];
+    [self formatNavBar];
 }
 
 #pragma mark - UINavigationController delegate
 
-// THIS IS HOW WE UPDATE THE PROGRESS BAR
+// update the progress bar everytime the nav controller pushes/pops a VC
 -(void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated
 {
-    NSInteger currentNumberOfControllers = [navigationController.viewControllers count] -1;
-    [self.myProgressView setProgress:(currentNumberOfControllers/totalNumberOfViewControllers) animated:YES];
+    NSInteger currentNumberOfControllers = [navigationController.viewControllers count]-1;
+    [self.myProgressView setProgress:(currentNumberOfControllers/TOTAL_VIEW_CONTROLLERS) animated:YES];
     
-    NSLog(@"DID SHOW");
+    NSLog(@"navigationController:willShowViewController:");
     NSLog(@"STACK COUNT: %ld", [navigationController.viewControllers count]);
+}
+
+#pragma mark - Helpers
+
+-(void) formatNavBar
+{
+    [self.navController.navigationBar setTranslucent: NO];
+    [self.navController.navigationBar setBarTintColor: [UIColor DonorsChooseOrange]];
+    [self.navController.navigationBar setTitleTextAttributes:@{                                                                    NSForegroundColorAttributeName : [UIColor DonorsChooseGreyVeryLight],NSFontAttributeName:[UIFont fontWithName:DonorsChooseTitleBoldFont size:25]}];
 }
 
 @end
--- TeachersChoose/SpreadTheNewsViewController.m
+++ TeachersChoose/SpreadTheNewsViewController.m
@@ -8,20 +8,29 @@
 
 #import "SpreadTheNewsViewController.h"
 
-static NSString* TEXTVIEW_PLACEHOLDER = @"Tap here to begin your message";
+static NSString* const TEXTVIEW_PLACEHOLDER = @"Tap here to begin your message";
 
 @interface SpreadTheNewsViewController () <UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *saveMessageButton;
 @property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textViewHeightConstraint;
+
+@property (nonatomic) float amountToResizeTextView;
 
 @end
 
 @implementation SpreadTheNewsViewController
 
+
+#pragma mark - View LifeCycle
+
 - (void)viewDidLoad {
     self.textView.delegate = self;
+    self.textView.autocorrectionType = UITextAutocorrectionTypeNo;
     self.saveMessageButton.layer.cornerRadius = 10;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receivedKeyboardNotification:) name:UIKeyboardWillShowNotification object:nil];
+    
     [self setupKeyboardDismissalOnTouch];
     [self createInputAccessoryView];
     [super viewDidLoad];
@@ -31,6 +40,99 @@ static NSString* TEXTVIEW_PLACEHOLDER = @"Tap here to begin your message";
 {
     [super viewDidAppear: animated];
     [self presentAlert];
+}
+
+#pragma mark - Alert Controller
+
+-(void) presentAlert
+{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Brief Thank You Message" message:@"This note will be publicly viewable on your project page and cannot be changed.\n\nFor safety purposes DO NOT include your name, school name, location, etc." preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okayAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    
+    [alertController addAction: okayAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark - UITextViewDelegate
+
+-(void)textViewDidBeginEditing:(UITextView *)textView
+{
+    if ([textView.text isEqualToString:  TEXTVIEW_PLACEHOLDER])
+    {
+        textView.text = @"";
+        textView.textColor = [UIColor blackColor];
+        textView.textAlignment = NSTextAlignmentLeft;
+    }
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    if ([textView.text isEqualToString:@""]) {
+        textView.text = TEXTVIEW_PLACEHOLDER;
+        textView.textColor = [UIColor lightGrayColor];
+        textView.textAlignment = NSTextAlignmentCenter;
+    }
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        // amountToResizeTextView setup in receivedKeyboardNotification:
+        self.textViewHeightConstraint.constant += (self.amountToResizeTextView);
+        [self.view layoutIfNeeded];
+    }];
+}
+
+-(void) textViewDidChange:(UITextView *)textView
+{
+    if ([textView.text length] > 3) {
+        self.saveMessageButton.enabled = YES;
+        self.saveMessageButton.backgroundColor = [UIColor colorWithRed:0.106 green:0.761 blue:0.106 alpha:1.000];
+    }
+    else {
+        self.saveMessageButton.enabled = NO;
+        self.saveMessageButton.backgroundColor = [UIColor lightGrayColor];
+    }
+}
+
+#pragma mark - Keyboard / InputAccessoryView
+
+-(void) receivedKeyboardNotification:(NSNotification*) notification
+{
+    NSValue*keyboardFrame = [notification.userInfo objectForKey:UIKeyboardFrameEndUserInfoKey];
+    CGRect  keyboardRect = [keyboardFrame CGRectValue];
+    CGFloat keyboardTopY = keyboardRect.origin.y;
+//    NSLog(@"kbty: %f", keyboardTopY);
+    
+    CGFloat textViewTopY = self.textView.superview.frame.origin.y;
+    CGFloat textViewHeight = self.textView.frame.size.height;
+    CGFloat textViewBottomY = textViewTopY + textViewHeight;
+//    NSLog(@"tvby: %f", textViewBottomY);
+    
+    self.amountToResizeTextView = (textViewBottomY - keyboardTopY);
+//    NSLog(@"diff: %f", self.amountToResizeTextView);
+    
+//    NSLog(@"keyboard: %@", NSStringFromCGRect(keyboardRect));
+//    NSLog(@"supervw : %@", NSStringFromCGRect(self.textView.superview.frame));
+//    NSLog(@"textview: %@", NSStringFromCGRect(self.textView.frame));
+//    NSLog(@"IAV     : %@", NSStringFromCGRect(self.textView.inputAccessoryView.frame));
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        self.textViewHeightConstraint.constant -= (self.amountToResizeTextView);
+        [self.view layoutIfNeeded];
+    }];
+}
+
+-(void) createInputAccessoryView
+{
+    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
+
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self.textView action:@selector(resignFirstResponder)];
+
+    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    toolbar.items = @[flexibleSpace, doneButton];
+
+    self.textView.inputAccessoryView = toolbar;
 }
 
 -(void) setupKeyboardDismissalOnTouch
@@ -46,73 +148,5 @@ static NSString* TEXTVIEW_PLACEHOLDER = @"Tap here to begin your message";
 {
     [self.textView resignFirstResponder];
 }
-
-
-#pragma mark - UITextViewDelegate
-
--(void)textViewDidBeginEditing:(UITextView *)textView
-{
-    if ([textView.text isEqualToString:TEXTVIEW_PLACEHOLDER])
-    {
-        textView.text = @"";
-        textView.textColor = [UIColor blackColor];
-        textView.textAlignment = NSTextAlignmentLeft;
-    }
-}
-
--(void)textViewDidEndEditing:(UITextView *)textView
-{
-    if ([textView.text isEqualToString:@""]) {
-        textView.text = TEXTVIEW_PLACEHOLDER;
-        textView.textColor = [UIColor lightGrayColor];
-        textView.textAlignment = NSTextAlignmentCenter;
-    }
-}
-
--(void) textViewDidChange:(UITextView *)textView
-{
-    if ([textView.text length] > 3) {
-        self.saveMessageButton.enabled = YES;
-        self.saveMessageButton.backgroundColor = [UIColor colorWithRed:0.106 green:0.761 blue:0.106 alpha:1.000];
-    }
-    else {
-        self.saveMessageButton.enabled = NO;
-        self.saveMessageButton.backgroundColor = [UIColor lightGrayColor];
-    }
-}
-
--(void) presentAlert
-{
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Brief Thank You Message" message:@"This note will be publicly viewable on your project page and cannot be changed.\n\nFor safety purposes DO NOT include your name, school name, location, etc." preferredStyle:UIAlertControllerStyleAlert];
-    
-    UIAlertAction *okayAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
-    
-    [alertController addAction: okayAction];
-    
-    [self presentViewController:alertController animated:YES completion:nil];
-}
-
--(void) createInputAccessoryView
-{
-    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 30)];
-
-    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self.textView action:@selector(resignFirstResponder)];
-
-    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
-    
-    toolbar.items = @[flexibleSpace, doneButton];
-
-    self.textView.inputAccessoryView = toolbar;
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
